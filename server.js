b335300c2e2ea215775d988c8bb856b8669a0deb
dangquy95php@gmail.com <dangquy95php@gmail.com>
2020-12-25 22:15:46 +0700

    add

--- server.js
+++ server.js
@@ -113,6 +113,6 @@ console.log('da start server');
 //     console.log('Error connecting to database');
 // });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.DOMAIN}`);
+app.listen(app.get('port'), () => {
+    console.log(`Server running on port`, app.get('port'));
 });
